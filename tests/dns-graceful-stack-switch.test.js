600c823eb3d36f95d033717a7ad8234ab9f9dc20
Vsevolod Strukchinsky <floatdrop@yandex-team.ru>
2013-08-21 15:45:19 +0600

    remove test with lodash

--- tests/dns-graceful-stack-switch.test.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-var _ = require('lodash');
-
-describe('dns-gracefull-stack-switch', function() {
-	it('should load module with function', function() {
-		var dnsPatcher = require('..');
-		expect(_.isFunction(dnsPatcher));
-	});
-});
