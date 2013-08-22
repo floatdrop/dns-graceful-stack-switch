b8c31a6e7e2e97d6541cb5bc1a41684e01f5bea1
Vsevolod Strukchinsky <floatdrop@yandex-team.ru>
2013-08-22 13:39:12 +0600

    right error reporting and version switching

---
+++ tests/error.test.js
@@ -0,0 +1,20 @@
+var expect = require('chai').expect;
+var dns = require('dns');
+
+var host = "unknown host";
+
+var ipv4;
+var ipv6;
+
+describe("Test error reporting", function() {
+
+	it('Error should contain IPv4 and IPv6 errors', function(done) {
+		require('..')(6);
+		dns.lookup(host, function(err, address, family) {
+			expect(err).to.include("IPv4");
+			expect(err).to.include("IPv6");
+			done();
+		});
+	});
+
+});
--- tests/resolve.test.js
+++ tests/resolve.test.js
@@ -9,23 +9,23 @@ var ipv6;
 describe("Test resolving", function() {
 
 	it('resolve4 should resolve yandex.ru into same address as dns module', function(done) {
+		require('..')(null, true);
 		require('..')(4);
-		dns.resolve4(host, function(err, addresses) {
+		dns.lookup(host, function(err, address) {
 			expect(err).to.not.exist;
-			expect(addresses).to.exist;
-			addresses.sort();
-			expect(addresses).to.eql(ipv4);
+			expect(address).to.exist;
+			expect(ipv4).to.include(address);
 			done(err);
 		});		
 	});
 
 	it('resolve6 yandex.ru into same address as dns module', function(done) {
+		require('..')(null, true);
 		require('..')(6);
-		dns.resolve6(host, function(err, addresses) {
+		dns.lookup(host, function(err, address) {
 			expect(err).to.not.exist;
-			expect(addresses).to.exist;
-			addresses.sort();
-			expect(addresses).to.eql(ipv6);
+			expect(address).to.exist;
+			expect(ipv6).to.include(address);
 			done(err);
 		});		
 	});
