e2e93dab02adcbb708ba603726f541ed46f7bbfb
Maxim Ponomarev <maxvipon@gmail.com>
2015-05-25 14:59:24 +0500

    fix string type of err

--- tests/error.test.js
+++ tests/error.test.js
@@ -11,8 +11,8 @@ describe("Test error reporting", function() {
 	it('Error should contain IPv4 and IPv6 errors', function(done) {
 		require('..')(6);
 		dns.lookup(host, function(err, address, family) {
-			expect(err).to.include("IPv4");
-			expect(err).to.include("IPv6");
+			expect(err.message).to.include("IPv4");
+			expect(err.message).to.include("IPv6");
 			done();
 		});
 	});
--- tests/resolve.test.js
+++ tests/resolve.test.js
@@ -1,34 +1,12 @@
 var expect = require('chai').expect;
 var dns = require('dns');
 
-var host = "www.google.com";
+var host = "yandex.com";
 
 var ipv4;
 var ipv6;
 
 describe("Test resolving", function() {
-
-	it('resolve4 should resolve yandex.ru into same address as dns module', function(done) {
-		require('..')(null, true);
-		require('..')(4);
-		dns.lookup(host, function(err, address) {
-			expect(err).to.not.exist;
-			expect(address).to.exist;
-			expect(ipv4).to.include(address);
-			done(err);
-		});		
-	});
-
-	it('resolve6 yandex.ru into same address as dns module', function(done) {
-		require('..')(null, true);
-		require('..')(6);
-		dns.lookup(host, function(err, address) {
-			expect(err).to.not.exist;
-			expect(address).to.exist;
-			expect(ipv6).to.include(address);
-			done(err);
-		});		
-	});
 
 	before(function(done) {
 		var tasks = 2;
@@ -47,6 +25,28 @@ describe("Test resolving", function() {
 			ipv6.sort();
 			tasks -= 1;
 			if (!tasks) done();
+		});
+	});
+
+	it('resolve4 should resolve ' + host + ' into same address as dns module', function(done) {
+		require('..')(null, true);
+		require('..')(4);
+		dns.lookup(host, function(err, address) {
+			expect(err).to.not.exist;
+			expect(address).to.exist;
+			expect(ipv4).to.include(address);
+			done(err);
+		});
+	});
+
+	it('resolve6 should resolve ' + host + ' into same address as dns module', function(done) {
+		require('..')(null, true);
+		require('..')(6);
+		dns.lookup(host, function(err, address) {
+			expect(err).to.not.exist;
+			expect(address).to.exist;
+			expect(ipv6).to.include(address);
+			done(err);
 		});
 	});
 
